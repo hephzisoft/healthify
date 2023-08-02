479cbd64e386d5e7c14d30ea5bb38b2325ee730b
Hephzisoft <hephzisoft@gmail.com>
2023-08-02 22:22:03 +0100

    First commit

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+
+  static const routeName = '/splash-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+
+      ),
+    );
+  }
+}
