24c689d4997fe43e700cb2e84d34f2d8d2e18ede
Hephzisoft <hephzisoft@gmail.com>
2023-08-03 15:48:31 +0100

    added some changes

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
-
-
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-         child:   Text('Welcome to this Application')
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to this Application'),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: () {},
+                child: const Text('Get Started'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
