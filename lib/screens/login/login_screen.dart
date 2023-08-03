832af4d43092b26d64ec542b882f0b0e00b05c8d
Metro <metrodev05@gmail.com>
2023-08-03 16:57:11 +0100

    changes to the login _screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -12,33 +12,33 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-       return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: Column(children: [
-                  const Text('hey there', style: ,),
-                  ,
-                ]),
-                // children: [
-                //   const Text(
-                //     'Hey there',
-                //     style: TextStyle(
-                //         color: Colors.black87,
-                //        fontFamily: 'kanit',
-                //         fontWeight: FontWeight.w200,
-                //         fontSize: 20),
-                //   ),
-                // ],
-               
-
-                ),
-                ]
+    return Padding(
+  
+      padding: const EdgeInsets.all(10),
+      child: ListView(children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(10),
+          child: const Column(children: [
+            Text(
+              'hey there',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontFamily: 'kanit',
+                  fontWeight: FontWeight.w200,
+                  fontSize: 20),
             ),
-                
-                );
+            Text(
+              'WELCOME BACK',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontFamily: 'kanit',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12),
+            )
+          ]),
+        ),
+      ]),
+    );
   }
 }
