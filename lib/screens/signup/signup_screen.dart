c795ee2f07593c7f61aec0a8920c44b217f3e8d5
Hephzisoft <hephzisoft@gmail.com>
2023-08-04 12:29:13 +0100

    Finished the signup page

--- lib/screens/signup/signup_screen.dart
+++ lib/screens/signup/signup_screen.dart
@@ -18,228 +18,213 @@ class _SignupScreenState extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: SvgPicture.asset(logoSvg),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
-                  Text(
-                    'HEY THERE',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Create an Account',
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          fontSize: 27,
-                        ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(
-                  17,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: SvgPicture.asset(logoSvg),
                 ),
-                child: SingleChildScrollView(
-                  child: Form(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: TextFormField(
-                            textCapitalization: TextCapitalization.words,
-                            style: const TextStyle(
-                              fontFamily: 'kanit',
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: 'First Name',
-                              prefixIcon: const Icon(
-                                Icons.person,
-                                color: Colors.grey,
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                                borderSide: const BorderSide(),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: TextFormField(
-                            textCapitalization: TextCapitalization.words,
-                            style: const TextStyle(
-                              fontFamily: 'kanit',
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: 'Last Name',
-                              prefixIcon: const Icon(
-                                Icons.person,
-                                color: Colors.grey,
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            style: const TextStyle(
-                              fontFamily: 'kanit',
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: 'Email',
-                              prefixIcon: const Icon(
-                                Icons.mail,
-                                color: Colors.grey,
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: TextFormField(
-                            keyboardType: TextInputType.phone,
-                            style: const TextStyle(
-                              fontFamily: 'kanit',
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: 'Phone Number',
-                              prefixIcon: const Icon(
-                                Icons.call,
-                                color: Colors.grey,
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: TextFormField(
-                            textCapitalization: TextCapitalization.words,
-                            keyboardType: TextInputType.visiblePassword,
-                            style: const TextStyle(
-                              fontFamily: 'kanit',
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: 'Password',
-                              prefixIcon: const Icon(
-                                Icons.lock,
-                                color: Colors.grey,
-                              ),
-                              suffixIcon: IconButton(
-                                icon: const Icon(Icons.remove_red_eye,
-                                    color: Colors.grey),
-                                onPressed: () {},
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
+                Column(
                   children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: FilledButton(
-                        style: FilledButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 15,
-                            ),
-                            textStyle: const TextStyle(
-                              fontFamily: 'Bruno Asc',
-                              fontSize: 18,
-                            )),
-                        onPressed: () {},
-                        child: const Text('Register'),
-                      ),
+                    Text(
+                      'HEY THERE',
+                      style: Theme.of(context).textTheme.titleMedium,
                     ),
                     const SizedBox(
-                      height: 20,
+                      height: 30,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                            height: 1.0,
-                            color: Colors.grey,
+                    Text(
+                      'Create an Account',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            fontSize: 27,
                           ),
-                        ),
-                        Text(
-                          'Or',
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                            height: 1.0,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Don\'t have an account?',
-                          style: Theme.of(context).textTheme.titleSmall,
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushNamed(LoginScreen.routeName);
-                          },
-                          child: Text(
-                            'Login',
-                            style:
-                                Theme.of(context).textTheme.titleSmall?.copyWith(
-                                      color: primaryColor,
-                                    ),
-                          ),
-                        ),
-                      ],
-                    )
                   ],
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(
+                    17,
+                  ),
+                  child: Form(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: TextFormField(
+                              textCapitalization: TextCapitalization.words,
+                              style: const TextStyle(
+                                fontFamily: 'kanit',
+                                fontWeight: FontWeight.normal,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: 'Full Name',
+                                prefixIcon: const Icon(
+                                  Icons.person,
+                                  color: Colors.grey,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  borderSide: const BorderSide(),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              style: const TextStyle(
+                                fontFamily: 'kanit',
+                                fontWeight: FontWeight.normal,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: 'Email',
+                                prefixIcon: const Icon(
+                                  Icons.mail,
+                                  color: Colors.grey,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: TextFormField(
+                              keyboardType: TextInputType.phone,
+                              style: const TextStyle(
+                                fontFamily: 'kanit',
+                                fontWeight: FontWeight.normal,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: 'Phone Number',
+                                prefixIcon: const Icon(
+                                  Icons.call,
+                                  color: Colors.grey,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: TextFormField(
+                              textCapitalization: TextCapitalization.words,
+                              keyboardType: TextInputType.visiblePassword,
+                              style: const TextStyle(
+                                fontFamily: 'kanit',
+                                fontWeight: FontWeight.normal,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                prefixIcon: const Icon(
+                                  Icons.lock,
+                                  color: Colors.grey,
+                                ),
+                                suffixIcon: IconButton(
+                                  icon: const Icon(Icons.remove_red_eye,
+                                      color: Colors.grey),
+                                  onPressed: () {},
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: FilledButton(
+                          style: FilledButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 15,
+                              ),
+                              textStyle: const TextStyle(
+                                fontFamily: 'Bruno Asc',
+                                fontSize: 18,
+                              ),),
+                          onPressed: () {},
+                          child: const Text('Register'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                              height: 1.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            'Or',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                              height: 1.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 50,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don\'t have an account?',
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .pushNamed(LoginScreen.routeName);
+                            },
+                            child: Text(
+                              'Login',
+                              style:
+                                  Theme.of(context).textTheme.titleSmall?.copyWith(
+                                        color: primaryColor,
+                                      ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
